ff9862fb84c816ceb466041f0797c30c1b68e77a
yamanimurillo <yamani.murillo@gmail.com>
2020-10-21 12:15:49 -0500

    Baseline for Crown Clothing project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+import HomePage from './components/homepage/homepage.component';
+
+function App() {
+  return (
+    <div>
+      <HomePage></HomePage>
+    </div>
+  );
+}
+
+export default App;
