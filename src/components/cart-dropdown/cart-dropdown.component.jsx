b449ef9dd75a9038171896c6b071854227e58a0d
yamanimurillo <yamani.murillo@gmail.com>
2021-01-27 15:24:25 -0600

    created car-item component and connected our cart dropdown with cartItems reducer

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -2,19 +2,16 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
 
 import './cart-dropdown.styles.scss';
 
 const CartDropdown = ({ cartItems }) => {
     return <div className='cart-dropdown'>
-        <div className='cart-items'> 
-            {cartItems.map((item, value) => {
-
-                return (
-                    <div>
-                        {item.name}
-                    </div>);
-            })}
+        <div className='cart-items'>
+            {
+                cartItems.map(cartItem => (<CartItem item={cartItem}></CartItem>))
+            }
         </div>
         <CustomButton>GO TO CHECKOUT</CustomButton>
     </div>
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -5,10 +5,19 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
     });
 
     if (existingCartItem) {
-        existingCartItem.quantity = existingCartItem.quantity + 1;
-    } else {
-        cartItems.push([{ ...cartItemToAdd, quantity: 1}]);
+        return cartItems.map(cartItem => {
+            return cartItem.id === cartItemToAdd.id
+                ? { ...cartItem, quantity: cartItem.quantity + 1 }
+                : { ...cartItem }
+                
+        })
     }
 
-    return [...cartItems];
+    // if (existingCartItem) {
+    //     existingCartItem.quantity = existingCartItem.quantity + 1;
+    // } else {
+    //     cartItems.push([{ ...cartItemToAdd, quantity: 1}]);
+    // }
+
+    return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
